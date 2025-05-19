f5167c5eeb884fa0bb28da9385ae1f985d2077e9
harishmugi <harishmugimugi@gmail.com>
2025-05-19 10:05:48 +0530

    Update index.ts== remove ||'*'

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ const start = async () => {
         return h
           .response()
           .code(200)
-          .header('Access-Control-Allow-Origin', request.headers.origin || '*')
+          .header('Access-Control-Allow-Origin', request.headers.origin)
           .header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
           .header(
             'Access-Control-Allow-Headers',
