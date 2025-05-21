d9557b2cfd5996ecce81af256726cd155bde8553
harishmugi <harishmugimugi@gmail.com>
2025-05-21 10:35:31 +0530

    check the worker after changing the host to 0.0.0.0

--- dist/src/queue/employeeQueue.js
+++ dist/src/queue/employeeQueue.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.employeeQueue = exports.redisConnection = void 0;
 const bullmq_1 = require("bullmq");
 exports.redisConnection = {
-    host: process.env.REDIS_HOST,
+    host: "0.0.0.0",
     port: parseInt(process.env.REDIS_PORT || '6379'),
     password: process.env.REDIS_PASSWORD,
     tls: {}, // Required for Upstash Redis
