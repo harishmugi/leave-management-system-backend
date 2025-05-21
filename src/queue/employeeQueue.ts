89c2e1ff0229fe50efdd25e875ea6996d3357cae
harishmugi <harishmugimugi@gmail.com>
2025-05-21 11:17:49 +0530

    revert 0.0.0.0

--- src/queue/employeeQueue.ts
+++ src/queue/employeeQueue.ts
@@ -2,7 +2,7 @@ import { Queue } from 'bullmq';
 import { RedisOptions } from 'ioredis';
 
 export const redisConnection: RedisOptions = {
-  host: "0.0.0.0",
+  host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || '6379'),
   password: process.env.REDIS_PASSWORD,
   tls: {}, // Required for Upstash Redis
