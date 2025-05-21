afca9507e71ab8f00a7504e2a41632f360a7f315
harishmugi <harishmugimugi@gmail.com>
2025-05-21 11:43:38 +0530

    after adding th eurl instead of redis object

--- src/queue/employeeQueue.ts
+++ src/queue/employeeQueue.ts
@@ -1,5 +1,7 @@
 import { Queue } from 'bullmq';
-
+import dotenv from 'dotenv'
+dotenv.config();
+console.log('asdasd',process.env.REDIS_URL)
 export const employeeQueue = new Queue('employee-create-queue', {
   connection: {
     url: process.env.REDIS_URL,
